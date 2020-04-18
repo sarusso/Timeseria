78d87664298b430a3ec624c677b23f69a391d641
Stefano Alberto Russo <stefano.russo@gmail.com>
2020-04-18 12:17:02 +0200

    Added the operators package with the Slotter and its tests.

---
+++ timeseria/operators.py
@@ -0,0 +1,328 @@
+from .time import TimeSpan, dt_from_s, s_from_dt
+from .datastructures import DataTimeSlot, DataTimeSlotSerie, TimePoint, DataTimePointSerie
+from .utilities import compute_coverage
+
+# Setup logging
+import logging
+logger = logging.getLogger(__name__)
+
+HARD_DEBUG = False
+
+
+#======================
+#  Utilities
+#======================
+
+def almostequal(one, two):
+    if 0.95 < (one / two) <= 1.05:
+        return True
+    else:
+        return False 
+
+
+
+#======================
+#  Slotter
+#======================
+
+class Slotter(object):
+    
+    def __init__(self, timeSpan):
+        
+        # Check vand set alid timeSpacn
+        if not isinstance(timeSpan, TimeSpan):
+            timeSpan = TimeSpan(timeSpan)
+        self.timeSpan = timeSpan
+
+    @classmethod
+    def _detect_dataPoints_validity(cls, dataTimePointSerie):
+    
+        diffs={}
+        prev_dataTimePoint=None
+        for dataTimePoint in dataTimePointSerie:
+            if prev_dataTimePoint is not None:
+                diff = dataTimePoint.t - prev_dataTimePoint.t
+                if diff not in diffs:
+                    diffs[diff] = 1
+                else:
+                    diffs[diff] +=1
+            prev_dataTimePoint = dataTimePoint
+        
+        # Iterate until the diffs are not too spread, then pick the maximum.
+        i=0
+        while almostequal(len(diffs), len(dataTimePointSerie)):
+            or_diffs=diffs
+            diffs={}
+            for diff in or_diffs:
+                diff=round(diff)
+                if diff not in diffs:
+                    diffs[diff] = 1
+                else:
+                    diffs[diff] +=1            
+            
+            if i > 10:
+                raise Exception('Cannot automatically detect dataPoints validity')
+        
+        most_common_diff_total = 0
+        most_common_diff = None
+        for diff in diffs:
+            if diffs[diff] > most_common_diff_total:
+                most_common_diff_total = diffs[diff]
+                most_common_diff = diff
+        return(most_common_diff)
+
+
+    def compute_slot(self, dataTimePointSerie, start_t, end_t, validity, timezone):
+
+        # Compute coverage
+        slot_coverage = compute_coverage(dataTimePointSerie,
+                                         from_t   = start_t,
+                                         to_t     = end_t,
+                                         validity = validity)
+        
+        # Compute data
+        keys = None
+        avgs = {}
+        for dataTimePoint in dataTimePointSerie:
+            if not keys:
+                keys=dataTimePoint.data.keys()
+            for key in keys:
+                if key not in avgs:
+                    avgs[key] = 0
+                avgs[key] += dataTimePoint.data[key]
+        
+        slot_data = {key:avgs[key]/len(dataTimePointSerie) for key in keys}
+        slot_data['data_loss'] = ((1-slot_coverage))
+
+        # Create the DataTimeSlot
+        dataTimeSlot = DataTimeSlot(start = TimePoint(t=start_t, tz=timezone),
+                                    end   = TimePoint(t=end_t, tz=timezone),
+                                    data  = slot_data,
+                                    coverage=slot_coverage)
+        
+        return dataTimeSlot
+
+
+
+    def process(self, dataTimePointSerie, from_t=None, to_t=None, validity=None, force_close_last=False, include_extremes=False):
+        ''' Start the slotting process. If start and/or end are not set, they are set automatically based on first and last points of the sereis'''
+
+        if not isinstance(dataTimePointSerie, DataTimePointSerie):
+            raise TypeError('Can process only DataTimePointSeries, got "{}"'.format(dataTimePointSerie.__class__.__name__))
+
+        if not dataTimePointSerie:
+            raise ValueError('Cannot process empty dataTimePointSerie')
+
+        if include_extremes:
+            if from_t is not None or to_t is not None:
+                raise ValueError('Setting "include_extremes" is not compatible with giving a from_t or a to_t')
+            from_rounding_method = 'floor'
+            to_rounding_method   = 'ceil'        
+        else:
+            from_rounding_method = 'ceil'
+            to_rounding_method   = 'floor'
+
+        # Set "from" if not set, otherwise check for consistency # TODO: move to steaming
+        if from_t is None:
+            from_t = dataTimePointSerie[0].t
+            from_dt = dt_from_s(from_t)
+            # Is the point already rounded to the time span or do we have to round it ourselves?
+            if not from_dt == self.timeSpan.round_dt(from_dt):
+                from_dt = self.timeSpan.round_dt(from_dt, how=from_rounding_method)
+                from_t  = s_from_dt(from_dt)
+        else:
+            from_dt = dt_from_s(from_t)
+            if from_dt != self.timeSpan.round_dt(from_dt):
+                raise ValueError('Sorry, provided from_t is not consistent with the timeSpan of "{}" (Got "{}")'.format(self.timeSpan, from_t))
+
+        # Set "to" if not set, otherwise check for consistency # TODO: move to streaming
+        if to_t is None:
+            to_t = dataTimePointSerie[-1].t
+            to_dt = dt_from_s(to_t)
+            # Is the point already rounded to the time span or do we have to round it ourselves?
+            if not to_dt == self.timeSpan.round_dt(to_dt):
+                to_dt = self.timeSpan.round_dt(to_dt, how=to_rounding_method)
+                to_t  = s_from_dt(to_dt)
+        else:
+            to_dt = dt_from_s(to_t)
+            if to_dt != self.timeSpan.round_dt(to_dt):
+                raise ValueError('Sorry, provided to_t is not consistent with the timeSpan of "{}" (Got "{}")'.format(self.timeSpan, to_t))
+        
+        # Automatically detect validity if not set
+        if validity is None:
+            validity = self._detect_dataPoints_validity(dataTimePointSerie)
+            logger.info('Auto-detected dataTimePoints validity: %s s', validity)
+        
+
+        
+        force_close_last = force_close_last # TODO: set to true if inverting floor and ceil above)
+
+        logger.debug('Started slotter from "%s" (%s) to "%s" (%s)', from_dt, from_t, to_dt, to_t)
+
+        if from_dt >= to_dt:
+            raise ValueError('Sorry, from is >= to! (from_t={}, to_t={})'.format(from_t, to_t))
+
+        # Set some support vars
+        slot_start_t       = None
+        slot_end_t         = None
+        prev_dataTimePoint = None
+        working_serie      = DataTimePointSerie()
+        process_ended      = False
+        dataTimeSlotSerie  = DataTimeSlotSerie()
+
+        # Set timezone
+        timezone  = dataTimePointSerie.tz
+        logger.debug('Using timezone "%s"', timezone)
+       
+        # Counters
+        count = 0
+
+        # Now go trough all the data in the time series        
+        for dataTimePoint in dataTimePointSerie:
+
+            logger.debug('Processing %s', dataTimePoint)
+
+            # Increase counter
+            count += 1
+            
+            # Set start_dt if not already done TODO: implement it correctly
+            #if not from_dt:
+            #    from_dt = self.timeSpan.timeInterval.round_dt(dataTimePoint.dt) if rounded else dataTimePoint.dt
+            
+            # Pretend there was a slot before if we are at the beginning. TOOD: improve me.
+            if not slot_end_t:   
+                slot_end_t = from_t
+
+            # First, check if we have some points to discard at the beginning       
+            if dataTimePoint.t < from_t:
+                # If we are here it means we are going data belonging to a previous slot
+                # (probably just spare data loaded to have access to the prev_datapoint)  
+                prev_dataTimePoint = dataTimePoint
+                continue
+
+            # Similar concept for the end
+            # TODO: what if we are in streaming mode? add if to_t is not None?
+            if dataTimePoint.t >= to_t:
+                if process_ended:
+                    continue
+
+            # The following procedure works in general for slots at the beginning and in the middle.
+            # The approach is to detect if the current slot is "outdated" and spin a new one if so.
+
+            if dataTimePoint.t > slot_end_t:
+                # If the current slot is outdated:
+                         
+                # 1) Add this last point to the dataTimePointSerie:
+                working_serie.append(dataTimePoint)
+                 
+                #2) keep spinning new slots until the current data point falls in one of them.
+                
+                # NOTE: Read the following "while" more as an "if" which can also lead to spin multiple
+                # slot if there are empty slots between the one being closed and the dataTimePoint.dt.
+                # TODO: leave or remove the above if for code readability?
+                
+                while slot_end_t < dataTimePoint.t:
+                    logger.debug('Checking for end {} with point {}'.format(slot_end_t, dataTimePoint.t))
+                    # If we are in the pre-first slot, just silently spin a new slot:
+                    if slot_start_t is not None:
+                        
+                        # Append last point. Can be appended to multiple slots, this is normal since
+                        # the empty slots in the middle will have only a far prev and a far next.
+                        # can also be appended several times if working_serie is not reset (looping in the while)
+                        if dataTimePoint not in working_serie:
+                            working_serie.append(dataTimePoint)
+  
+                        logger.debug('This slot (start={}, end={}) is closed, now aggregating it..'.format(slot_start_t, slot_end_t))
+                        
+                        logger.debug('working_serie first point dt: %s', working_serie[0].dt)
+                        logger.debug('working_serie  last point dt: %s', working_serie[-1].dt)
+
+                        
+                        # Compute slot...
+                        dataTimeslot = self.compute_slot(working_serie,
+                                                         start_t  = slot_start_t,
+                                                         end_t    = slot_end_t,
+                                                         validity = validity,
+                                                         timezone = timezone)
+                        
+                        # .. and append results 
+                        dataTimeSlotSerie.append(dataTimeslot)
+
+
+                    # Create a new slot. This is where all the "conventional" time logic kicks-in, and where the time zone is required.
+                    slot_start_t = slot_end_t
+                    slot_end_t   = s_from_dt(dt_from_s(slot_start_t, tz=timezone) + self.timeSpan)
+                    
+                    # Create a new working_serie as part of the "create a new slot" procedure
+                    working_serie = DataTimePointSerie()
+                    
+                    # Append the previous prev_dataTimePoint to the new DataTimeSeries
+                    if prev_dataTimePoint:
+                        working_serie.append(prev_dataTimePoint)
+
+                    logger.debug('Spinned a new slot (start={}, end={})'.format(slot_start_t, slot_end_t))
+                    
+                # If last slot mark process as completed (and aggregate last slot if necessary)
+                if dataTimePoint.dt >= to_dt:
+
+                    # Edge case where we would otherwise miss the last slot
+                    if dataTimePoint.dt == to_dt:
+                        
+                        # Compute slot...
+                        dataTimeslot = self.compute_slot(working_serie,
+                                                         start_t  = slot_start_t,
+                                                         end_t    = slot_end_t,
+                                                         validity = validity,
+                                                         timezone = timezone)
+                        
+                        # .. and append results 
+                        dataTimeSlotSerie.append(dataTimeslot)
+
+                    process_ended = True
+                    
+
+            # Append this point to the working serie
+            working_serie.append(dataTimePoint)
+            
+            # ..and save as previous point
+            prev_dataTimePoint =  dataTimePoint           
+
+
+        # Last slots
+        if process_ended and force_close_last: # or method = overset (vs subset that is the standard)
+
+            # 1) Close the last slot and aggreagte it. You should never do it unless you knwo what you are doing
+            if working_serie:
+    
+                logger.debug('This slot (start={}, end={}) is closed, now aggregating it..'.format(slot_start_t, slot_end_t))
+      
+                # Compute slot...
+                dataTimeslot = self.compute_slot(working_serie,
+                                                 start_t  = slot_start_t,
+                                                 end_t    = slot_end_t,
+                                                 validity = validity,
+                                                 timezone = timezone)
+                
+                # .. and append results 
+                dataTimeSlotSerie.append(dataTimeslot)
+
+            # 2) Handle missing slots until the requested end (end_dt)
+            # TODO: Implement it. Sure?
+
+        logger.debug('Aggregation process ended, processed {} DataTimePoints.'.format(count))
+ 
+        dataTimeSlotSerie.timeSpan = self.timeSpan
+ 
+        return dataTimeSlotSerie
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ timeseria/tests/test_operators.py
@@ -0,0 +1,244 @@
+import unittest
+import os
+from ..datastructures import Point, TimePoint, DataPoint, DataTimePoint
+from ..datastructures import Serie, DataPointSerie, TimePointSerie, DataTimePointSerie
+from ..storages import CSVFileStorage
+from ..operators import Slotter
+from ..time import TimeSpan, dt, s_from_dt, dt_from_s, dt_from_str
+from ..exceptions import InputException
+
+# Setup logging
+import logging
+logging.basicConfig(level=os.environ.get('LOGLEVEL') if os.environ.get('LOGLEVEL') else 'CRITICAL')
+
+# Set test data path
+TEST_DATA_PATH = '/'.join(os.path.realpath(__file__).split('/')[0:-1]) + '/test_data/'
+
+
+class TestSlotter(unittest.TestCase):
+
+
+    def setUp(self):       
+        
+        # All the following time series have point with validity=1m
+        
+        # Time series from 16:58:00 to 17:32:00 (Europe/Rome)
+        self.dataTimePointSerie1 = DataTimePointSerie()
+        start_t = 1436022000 - 120
+        for i in range(35):
+            dataTimePoint = DataTimePoint(t = start_t + (i*60),
+                                          data = {'temperature': 154+i})
+            self.dataTimePointSerie1.append(dataTimePoint)
+        
+        # Time series from 17:00:00 to 17:30:00 (Europe/Rome)
+        self.dataTimePointSerie2 = DataTimePointSerie()
+        start_t = 1436022000
+        for i in range(34):
+            dataTimePoint = DataTimePoint(t    = start_t + (i*60),
+                                          data = {'temperature': 154+i})
+            self.dataTimePointSerie2.append(dataTimePoint)
+
+        # Time series from 17:00:00 to 17:20:00 (Europe/Rome)
+        self.dataTimePointSerie3 = DataTimePointSerie()
+        start_t = 1436022000 - 120
+        for i in range(23):
+            dataTimePoint = DataTimePoint(t    = start_t + (i*60),
+                                          data = {'temperature': 154+i})
+            self.dataTimePointSerie3.append(dataTimePoint) 
+        
+        # Time series from 17:10:00 to 17:30:00 (Europe/Rome)
+        self.dataTimePointSerie4 = DataTimePointSerie()
+        start_t = 1436022000 + 600
+        for i in range(21):
+            dataTimePoint = DataTimePoint(t    = start_t + (i*60),
+                                          data = {'temperature': 154+i})
+            self.dataTimePointSerie4.append(dataTimePoint)
+
+        # Time series from 16:58:00 to 17:32:00 (Europe/Rome)
+        self.dataTimePointSerie5 = DataTimePointSerie()
+        start_t = 1436022000 - 120
+        for i in range(35):
+            if i > 10 and i <21:
+                continue
+            dataTimePoint = DataTimePoint(t    = start_t + (i*60),
+                                          data = {'temperature': 154+i})
+            self.dataTimePointSerie5.append(dataTimePoint)
+
+        # The following time series has point with validity=15m
+
+        # Time series from 2019,10,1,1,0,0 to 2019,10,1,6,0,0 (Europe/Rome)
+        from_dt  = dt(2019,10,1,1,0,0, tzinfo='Europe/Rome')
+        to_dt    = dt(2019,10,1,6,0,0, tzinfo='Europe/Rome')
+        timeSpan = TimeSpan('15m') 
+        self.dataTimePointSerie6 = DataTimePointSerie()
+        slider_dt = from_dt
+        count = 0
+        while slider_dt < to_dt:
+            if count not in [1, 6, 7, 8, 9, 10]:
+                dataTimePoint = DataTimePoint(t    = s_from_dt(slider_dt),
+                                              data = {'temperature': 154+count},
+                                              tz   = 'Europe/Rome')
+                self.dataTimePointSerie6.append(dataTimePoint)
+            slider_dt = slider_dt + timeSpan
+            count += 1
+
+        # The following time series has point with validity=1h
+
+        # Time series from 2019,10,24,0,0,0 to 2019,10,31,0,0,0 (Europe/Rome), DST off -> 2 AM repeated
+        from_dt  = dt(2019,10,24,0,0,0, tzinfo='Europe/Rome')
+        to_dt    = dt(2019,10,31,0,0,0, tzinfo='Europe/Rome')
+        timeSpan = TimeSpan('1h') 
+        self.dataTimePointSerie7 = DataTimePointSerie()
+        slider_dt = from_dt
+        count = 0
+        while slider_dt < to_dt:
+            if count not in []:
+                dataTimePoint = DataTimePoint(t    = s_from_dt(slider_dt),
+                                              data = {'temperature': 154+count},
+                                              tz   = 'Europe/Rome')
+                self.dataTimePointSerie7.append(dataTimePoint)
+            slider_dt = slider_dt + timeSpan
+            count += 1
+
+
+
+
+    def test_init(self):
+        
+        Slotter('1h')
+        Slotter(TimeSpan('1h'))
+        
+        with self.assertRaises(InputException):
+            Slotter('NO')
+        
+        
+
+    def test_detect_dataPoints_validity(self):
+
+        dataTimePointSerie = CSVFileStorage(TEST_DATA_PATH + '/csv/humitemp_short.csv').get()
+        self.assertEqual(Slotter._detect_dataPoints_validity(dataTimePointSerie), 61)
+        
+        dataTimePointSerie = CSVFileStorage(TEST_DATA_PATH + '/csv/humitemp_long.csv').get()
+        self.assertEqual(Slotter._detect_dataPoints_validity(dataTimePointSerie), 61)
+
+        dataTimePointSerie = CSVFileStorage(TEST_DATA_PATH + '/csv/shampoo_sales.csv', time_column = 'Month', time_format = '%y-%m').get()
+        self.assertEqual(Slotter._detect_dataPoints_validity(dataTimePointSerie), 2678400)
+        # TODO: 2678400/60/60/24 = 31 --> detect month? same for day, week, year?
+
+        dataTimePointSerie = CSVFileStorage(TEST_DATA_PATH + '/csv/temperature.csv').get()
+        self.assertEqual(Slotter._detect_dataPoints_validity(dataTimePointSerie), 600)
+
+        dataTimePointSerie = CSVFileStorage(TEST_DATA_PATH + '/csv/temp_short_1h.csv').get()
+        self.assertEqual(Slotter._detect_dataPoints_validity(dataTimePointSerie), 3600)
+
+
+
+    def test_process(self):
+        
+        # Time series from 16:58:00 to 17:32:00 (Europe/Rome), no from/to, extremes excluded (default)
+        slotter = Slotter('10m')
+        dataTimeSlotSerie = slotter.process(self.dataTimePointSerie1)
+        self.assertEqual(len(dataTimeSlotSerie), 3)
+        self.assertEqual(dataTimeSlotSerie[0].start.dt, dt_from_str('2015-07-04 17:00:00+02:00'))
+        self.assertEqual(dataTimeSlotSerie[1].start.dt, dt_from_str('2015-07-04 17:10:00+02:00'))
+        self.assertEqual(dataTimeSlotSerie[2].start.dt, dt_from_str('2015-07-04 17:20:00+02:00'))
+ 
+        # Time series from 16:58:00 to 17:32:00 (Europe/Rome), no from/to, extremes included.
+        slotter = Slotter('10m')
+        dataTimeSlotSerie = slotter.process(self.dataTimePointSerie1, include_extremes = True)
+        self.assertEqual(len(dataTimeSlotSerie), 4)
+        self.assertEqual(dataTimeSlotSerie[0].start.dt, dt_from_str('2015-07-04 16:50:00+02:00'))
+        self.assertEqual(dataTimeSlotSerie[1].start.dt, dt_from_str('2015-07-04 17:00:00+02:00'))
+        self.assertEqual(dataTimeSlotSerie[2].start.dt, dt_from_str('2015-07-04 17:10:00+02:00'))
+        self.assertEqual(dataTimeSlotSerie[3].start.dt, dt_from_str('2015-07-04 17:20:00+02:00'))
+        # TODO: the following missing is a bug..
+        #self.assertEqual(dataTimeSlotSerie[4].start.dt, dt_from_str('2015-07-04 17:30:00+02:00'))
+ 
+        # Time series from 16:58:00 to 17:32:00 (Europe/Rome), from 15:50 to 17:40
+        slotter = Slotter('10m')
+        dataTimeSlotSerie = slotter.process(self.dataTimePointSerie1,
+                                            from_t = s_from_dt(dt_from_str('2015-07-04 16:50:00+02:00')),
+                                            to_t   = s_from_dt(dt_from_str('2015-07-04 17:40:00+02:00')))
+ 
+        self.assertEqual(len(dataTimeSlotSerie), 4)
+        self.assertEqual(dataTimeSlotSerie[0].start.dt, dt_from_str('2015-07-04 16:50:00+02:00'))
+        self.assertEqual(dataTimeSlotSerie[1].start.dt, dt_from_str('2015-07-04 17:00:00+02:00'))
+        self.assertEqual(dataTimeSlotSerie[2].start.dt, dt_from_str('2015-07-04 17:10:00+02:00'))
+        self.assertEqual(dataTimeSlotSerie[3].start.dt, dt_from_str('2015-07-04 17:20:00+02:00'))
+        # TODO: the following missing is a bug..
+        #self.assertEqual(dataTimeSlotSerie[4].start.dt, dt_from_str('2015-07-04 17:30:00+02:00'))
+  
+        # Time series from 2019,10,1,1,0,0 to 2019,10,1,6,0,0 (Europe/Rome), validity=15m
+        slotter = Slotter('1h')
+        dataTimePointSerie = self.dataTimePointSerie6
+        dataTimeSlotSerie = slotter.process(dataTimePointSerie)
+        self.assertEqual(len(dataTimeSlotSerie), 4)
+        self.assertEqual(str(dataTimeSlotSerie[0].start.dt), str('2019-10-01 01:00:00+02:00'))
+        self.assertEqual(str(dataTimeSlotSerie[1].start.dt), str('2019-10-01 02:00:00+02:00'))
+        self.assertEqual(str(dataTimeSlotSerie[2].start.dt), str('2019-10-01 03:00:00+02:00'))
+        self.assertEqual(str(dataTimeSlotSerie[3].start.dt), str('2019-10-01 04:00:00+02:00'))
+
+        # Test changing timezone of the serie
+        slotter = Slotter('1h')
+        dataTimePointSerie = self.dataTimePointSerie6
+        dataTimePointSerie.tz = 'America/New_York'
+        dataTimeSlotSerie = slotter.process(dataTimePointSerie)
+        self.assertEqual(len(dataTimeSlotSerie), 4)
+        self.assertEqual(str(dataTimeSlotSerie[0].start.dt), str('2019-09-30 19:00:00-04:00'))
+        self.assertEqual(str(dataTimeSlotSerie[1].start.dt), str('2019-09-30 20:00:00-04:00'))
+        self.assertEqual(str(dataTimeSlotSerie[2].start.dt), str('2019-09-30 21:00:00-04:00'))
+        self.assertEqual(str(dataTimeSlotSerie[3].start.dt), str('2019-09-30 22:00:00-04:00'))
+        
+        # Time series from 2019,10,24,0,0,0 to 2019,10,31,0,0,0 (Europe/Rome), DST off -> 2 AM repeated
+        slotter = Slotter('1h')
+        dataTimePointSerie = self.dataTimePointSerie7
+        dataTimeSlotSerie = slotter.process(dataTimePointSerie)
+
+        self.assertEqual(len(dataTimeSlotSerie), 168)
+        self.assertEqual(str(dataTimeSlotSerie[73].start.dt), str('2019-10-27 01:00:00+02:00'))
+        self.assertEqual(str(dataTimeSlotSerie[74].start.dt), str('2019-10-27 02:00:00+02:00'))
+        self.assertEqual(str(dataTimeSlotSerie[75].start.dt), str('2019-10-27 02:00:00+01:00'))
+        self.assertEqual(str(dataTimeSlotSerie[76].start.dt), str('2019-10-27 03:00:00+01:00'))
+
+
+        # Work in progress in the following
+        #print('--------------------------------------------')
+        #for item in self.dataTimePointSerie6: print(item)
+        #print('--------------------------------------------')
+        #for i, item in enumerate(dataTimeSlotSerie): print(i, item)
+        #print('--------------------------------------------')
+       
+        # Time series from 2019,10,24,0,0,0 to 2019,10,31,0,0,0 (Europe/Rome), DST off -> 2 AM repeated
+        #dataTimeSlotSerie = slotter.process(self.dataTimePointSerie7, from_t=1571868600, to_t=1571873400)
+        #for item in dataTimeSlotSerie:
+        #    print(item)
+        # This is a downsampling, there are coverage=None.. TOOD: fix me
+ 
+        # 1-day sloter TODO: add all the "logical" time part, nothing works here...
+        #slotter = Slotter('1D')
+        # Time series from 2019,10,24,0,0,0 to 2019,10,31,0,0,0 (Europe/Rome), DST off -> 2 AM repeated
+        #dataTimeSlotSerie = slotter.process(self.dataTimePointSerie7)
+        #for item in dataTimeSlotSerie:
+        #    print(item)
+
+
+       
+#     def test_e2e(self):
+#         slotter = Slotter('1h')
+#         storage2 = CSVFileStorage(TEST_DATA_PATH + '/csv/humitemp_long.csv', skip_errors=True)
+#         dataTimePointSerie2 = storage2.get()
+#         dataTimeSlotSerie = slotter.process(dataTimePointSerie2)
+# 
+#         for item in dataTimeSlotSerie:
+#             print('{} - data={}'.format(item, item.data, item.coverage))
+
+
+
+
+
+
+
+
+
+
+
