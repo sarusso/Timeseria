a134df75ca25967e9e7c743ae228e3c01b918bc3
Stefano Alberto Russo <stefano.russo@gmail.com>
2021-12-07 23:32:13 +0100

    Fixed the multiarch manifest script.

--- containers/Ubuntu_20.04/create_multiarch_manifest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker manifest create \
-docker.io/sarusso/timeseria \
---amend docker.io/sarusso/timeseria-amd64 \
---amend docker.io/sarusso/timeseria-arm64v8
-
-# docker manifest push docker.io/sarusso/timeseria
---
+++ containers/Ubuntu_20.04/push_multiarch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+USER="sarusso"
+VERSION="v0.1.3"
+
+echo "Tagging..."
+docker tag timeseria-amd64 docker.io/$USER/timeseria-amd64:$VERSION
+docker tag timeseria-arm64v8 docker.io/$USER/timeseria-arm64v8:$VERSION
+
+echo "Pushing images"
+docker push docker.io/$USER/timeseria-amd64:$VERSION
+docker push docker.io/$USER/timeseria-arm64v8:$VERSION
+
+echo "Creating manifest..."
+docker manifest create \
+docker.io/$USER/timeseria:$VERSION \
+--amend docker.io/$USER/timeseria-amd64:$VERSION \
+--amend docker.io/$USER/timeseria-arm64v8:$VERSION
+
+echo "Pushing manifest"
+docker manifest push docker.io/$USER/timeseria:$VERSION
