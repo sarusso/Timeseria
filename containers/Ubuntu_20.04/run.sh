baebc7a8623a80d763db4b98db0cea599fc481e1
Stefano Alberto Russo <stefano.russo@gmail.com>
2021-12-08 14:28:41 +0100

    Changed behaviour for the container: now  mounting the codebase as a volume by default.

--- containers/Ubuntu_20.04/run.sh
+++ containers/Ubuntu_20.04/run.sh
@@ -9,11 +9,11 @@ echo -e  "\n==============================="
 echo -e  "|  Running Docker container   |"
 echo -e  "===============================\n"
 
-if [[ "x$LIVE" == "xTrue" ]]; then
-    echo "Running with live code changes"
-    docker run -p8888:8888 -v $PWD:/opt/Timeseria -v $PWD/notebooks:/notebooks -it timeseria
-else
-    echo "Running without live code changes. Use LIVE=True to enable them."
+if [[ "x$LIVE" == "xFalse" ]]; then
+    echo "Running without live code changes."
     docker run -p8888:8888 -it timeseria
+else
+    echo "Running with live code changes. Use LIVE=False to disable."
+    docker run -p8888:8888 -v $PWD:/opt/Timeseria -v $PWD/notebooks:/notebooks -it timeseria
 fi
 
