7909a9203f9d1694be74674c2249ff6200e6fc2a
Stefano Alberto Russo <stefano.russo@gmail.com>
2021-12-18 02:07:04 +0100

    Fixed a bug in the push_multiarch script which caused to append images to an already existent tag instead of replacing them.

--- containers/Ubuntu_20.04/push_multiarch.sh
+++ containers/Ubuntu_20.04/push_multiarch.sh
@@ -13,6 +13,7 @@ docker push docker.io/$USER/timeseria-amd64:$VERSION
 docker push docker.io/$USER/timeseria-arm64v8:$VERSION
 
 echo "Creating manifest..."
+docker manifest rm docker.io/$USER/timeseria:$VERSION
 docker manifest create \
 docker.io/$USER/timeseria:$VERSION \
 --amend docker.io/$USER/timeseria-amd64:$VERSION \
