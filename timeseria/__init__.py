96780d560c1e2f7642fd74a251daa33bf489999c
Stefano Alberto Russo <stefano.russo@gmail.com>
2025-05-28 12:08:51 +0200

    Added a trailing path separator when defining the TEST_DATASETS_PATH.

--- timeseria/__init__.py
+++ timeseria/__init__.py
@@ -1,3 +1,3 @@
 import os
 
-TEST_DATASETS_PATH = os.path.join(os.path.dirname(__file__), 'tests', 'test_data', 'csv')
+TEST_DATASETS_PATH = os.path.join(os.path.dirname(__file__), 'tests', 'test_data', 'csv', '')
